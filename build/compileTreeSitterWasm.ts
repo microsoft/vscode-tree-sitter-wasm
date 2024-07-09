5a7f28ae7948bfe7878b9fc6b16a725bfd11e50d
Alex Ross <alros@microsoft.com>
2024-07-09 17:22:26 +0200

    Fix d.ts file (#4)

--- build/compileTreeSitterWasm.ts
+++ build/compileTreeSitterWasm.ts
@@ -4,13 +4,18 @@ import * as path from 'path';
 
 export async function ensureTreeSitterWasm(repo: string, tag: string, clonePath: string, outputPath: string) {
     const repoSubpath = 'tree-sitter';
-    const command = `git clone --branch ${tag} ${repo} ${repoSubpath}`;
-	console.log(`Executing: ${command}`);
-	child_process.execSync(command, {
-		stdio: 'inherit',
-		cwd: clonePath,
-		encoding: 'utf8'
-	});
+    try {
+        await fs.rmdirSync(path.join(clonePath, repoSubpath), { recursive: true });
+    } catch (e) {
+        // Ignore.
+    }
+    const command = `git clone --branch ${tag} ${repo} ${repoSubpath} --depth 1 --single-branch --no-tags`;
+    console.log(`Executing: ${command}`);
+    child_process.execSync(command, {
+        stdio: 'inherit',
+        cwd: clonePath,
+        encoding: 'utf8'
+    });
 
     const treeSitterRepoPath = path.join(clonePath, repoSubpath);
     child_process.execSync('./script/build-wasm', {
@@ -21,9 +26,10 @@ export async function ensureTreeSitterWasm(repo: string, tag: string, clonePath:
 
     const builtWasmPath = path.join(treeSitterRepoPath, 'lib/binding_web');
     const jsFile = 'tree-sitter.js';
+    const dtsFile = 'tree-sitter-web.d.ts';
     const files = [
         'tree-sitter.wasm',
-        'tree-sitter-web.d.ts',
+        dtsFile,
         jsFile
     ];
     for (const file of files) {
@@ -33,19 +39,26 @@ export async function ensureTreeSitterWasm(repo: string, tag: string, clonePath:
         child_process.execSync(`cp ${src} ${dest}`);
     }
 
+    const dtsFilePath = path.join(outputPath, dtsFile);
+    let dtsFileContents = fs.readFileSync(dtsFilePath, 'utf-8');
+    dtsFileContents = dtsFileContents.substring(dtsFileContents.indexOf('{') + 1, dtsFileContents.lastIndexOf('export = Parser'));
+    dtsFileContents = dtsFileContents.replace('class Parser', 'export class Parser');
+    dtsFileContents = dtsFileContents.replace('namespace Parser', 'export namespace Parser');
+    fs.writeFileSync(dtsFilePath, dtsFileContents);
+
     const jsFilePath = path.join(outputPath, jsFile);
-    let jsFileConents = fs.readFileSync(jsFilePath, 'utf-8');
+    let jsFileContents = fs.readFileSync(jsFilePath, 'utf-8');
     // Make it UMD.
-    jsFileConents = `(function (global, factory) {
+    jsFileContents = `(function (global, factory) {
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
 		typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 			(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Parser = {}));
 })(this, (function () {
 
-${jsFileConents}
+${jsFileContents}
 
 	return { Parser: TreeSitter };
 }));`
 
-    fs.writeFileSync(jsFilePath, jsFileConents);
+    fs.writeFileSync(jsFilePath, jsFileContents);
 }
