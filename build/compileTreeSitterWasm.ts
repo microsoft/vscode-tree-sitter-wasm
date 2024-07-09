4ed1790a299ec106e31b6fe36bc05eb94cb556dd
Alex Ross <alros@microsoft.com>
2024-07-09 10:28:25 +0000

    Fix d.ts file

--- build/compileTreeSitterWasm.ts
+++ build/compileTreeSitterWasm.ts
@@ -21,9 +21,10 @@ export async function ensureTreeSitterWasm(repo: string, tag: string, clonePath:
 
     const builtWasmPath = path.join(treeSitterRepoPath, 'lib/binding_web');
     const jsFile = 'tree-sitter.js';
+    const dtsFile = 'tree-sitter-web.d.ts';
     const files = [
         'tree-sitter.wasm',
-        'tree-sitter-web.d.ts',
+        dtsFile,
         jsFile
     ];
     for (const file of files) {
@@ -33,19 +34,26 @@ export async function ensureTreeSitterWasm(repo: string, tag: string, clonePath:
         child_process.execSync(`cp ${src} ${dest}`);
     }
 
+    const dtsFilePath = path.join(outputPath, dtsFile);
+    let dtsFileContents = fs.readFileSync(dtsFilePath, 'utf-8');
+    dtsFileContents = dtsFileContents.substring(dtsFileContents.indexOf('{') + 1, dtsFileContents.lastIndexOf('export = Parser'));
+    dtsFileContents = dtsFileContents.replace('class Parser', 'export class Parser');
+    dtsFileContents = dtsFileContents.replace('namespace Parser', 'export namespace Parser');
+    fs.writeFileSync(dtsFilePath, dtsFileContents);
+
     const jsFilePath = path.join(outputPath, jsFile);
-    let jsFileConents = fs.readFileSync(jsFilePath, 'utf-8');
+    let jsFileContents = fs.readFileSync(jsFilePath, 'utf-8');
     // Make it UMD.
-    jsFileConents = `(function (global, factory) {
+    jsFileContents = `(function (global, factory) {
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
 		typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 			(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Parser = {}));
 })(this, (function () {
 
-${jsFileConents}
+${jsFileContents}
 
 	return { Parser: TreeSitter };
 }));`
 
-    fs.writeFileSync(jsFilePath, jsFileConents);
+    fs.writeFileSync(jsFilePath, jsFileContents);
 }
